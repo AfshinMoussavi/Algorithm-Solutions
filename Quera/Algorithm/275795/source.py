28af7f240baf5d9f325ccbdab0a761b9fd38d383
Afshin Moussavi <khodeafshin666@gmail.com>
2025-02-16 02:16:56 +0330

    Added solution for problem [275795] from [Quera]

---
+++ Quera/Algorithm/275795/source.py
@@ -0,0 +1,15 @@
+# Name: چاپ جمع
+# URL:  https://quera.org/problemset/275795
+
+x = int(input())
+
+result=  ""
+Sum = 0
+for number in range(1,x+1):
+    Sum += number
+    if number == x:
+        result += f"{number} = {Sum}"
+    else:
+        result += f"{number} + "
+    
+print(result)

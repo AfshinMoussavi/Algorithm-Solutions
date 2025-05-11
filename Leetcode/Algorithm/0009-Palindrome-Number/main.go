65bbf423e3828376fbbb4d6011a2061a8be07f41
Afshin Moussavi <khodeafshin666@gmail.com>
2025-05-11 14:16:19 +0330

    Solve problem [9] from [Leetcode]

---
+++ Leetcode/Algorithm/0009-Palindrome-Number/main.go
@@ -0,0 +1,26 @@
+// Name: 9. Palindrome Number
+// URL:  https://leetcode.com/problems/palindrome-number/description/
+
+package main
+
+import "strconv"
+
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	s := strconv.Itoa(x)
+
+	left, right := 0, len(s)-1
+	for left < right {
+		if s[left] != s[right] {
+			return false
+		}
+		left++
+		right--
+	}
+
+	return true
+}

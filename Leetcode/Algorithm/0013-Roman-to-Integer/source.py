3422dcb8a5e176fc7694c46f2d6b782774d3cf56
Afshin Moussavi <khodeafshin666@gmail.com>
2025-05-12 13:56:41 +0330

    Solve problem [13] from [Leetcode]

---
+++ Leetcode/Algorithm/0013-Roman-to-Integer/source.py
@@ -0,0 +1,28 @@
+# Name: 13. Roman to Integer
+# URL:  https://leetcode.com/problems/roman-to-integer/description/
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        prev = 0 
+
+        for ch in reversed(s): 
+            curr = roman[ch]
+            if curr < prev:
+                total -= curr
+            else:
+                total += curr
+            prev = curr
+
+        return total

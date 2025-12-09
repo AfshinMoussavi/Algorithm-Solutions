9eefee8d8c0de3898d1101b339b0cf30303d5191
Afshin Moussavi <khodeafshin666@gmail.com>
2025-12-09 06:12:32 +0330

    Solve problem [26] from [Leetcode]

--- Leetcode/Algorithm/0026-Remove-Duplicates-from-Sorted-Array/source.py
+++ Leetcode/Algorithm/0026-Remove-Duplicates-from-Sorted-Array/source.py
@@ -5,12 +5,11 @@ from operator import truediv
 
 class Solution:
     def removeDuplicates(self, nums: list) -> int:
-        unique = []
-        i = 0
-        while i < len(nums):
-            if nums[i] in unique:
-                nums.pop(i)
-            else:
-                unique.append(nums[i])
+        i = 1
+
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i-1]:
+                nums[i] = nums[j]
                 i += 1
-        return len(unique)
+
+        return i
